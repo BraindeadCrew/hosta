7bb77e8e82ad0f30eac59700146fa252953c1bb6
Maxime Tricoire <leiko@braindead.fr>
2013-09-06 12:44:52 +0200

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('hostaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'Pictures',
+      'Videos',
+      'Sounds'
+    ];
+  });
