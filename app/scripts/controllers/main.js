a379efc3f0425c0935c96b1be14b1a4273d4d759
Maxime Tricoire <max.tricoire@gmail.com>
2013-10-13 14:33:48 +0200

    improving rendering (still with static resources)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,46 @@
 
 angular.module('hostaApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+        $http.get('recents/recents.json').success(function(data) {
+            $scope.files = data;
+        });
+
+    $scope.files = [
+        {
+            name: 'foo.png',
+            typeico: 'img/image_ico.png',
+            url: 'uploads/foo.png',
+            cat: 'Joke',
+            date: (new Date).toLocaleString()
+        },
+        {
+            name: 'bar.jpg',
+            typeico: 'img/image_ico.png',
+            url: 'uploads/foo.png',
+            cat: 'Braindead',
+            date: (new Date).toLocaleString()
+        },
+        {
+            name: 'potato.yummy',
+            typeico: 'img/image_ico.png',
+            url: 'uploads/foo.png',
+            cat: 'Games',
+            date: (new Date).toLocaleString()
+        }
+    ];
+
+    $scope.options = [
+        {
+            name: 'Joke',
+            id: 0
+        },
+        {
+            name: 'Games',
+            id: 1
+        },
+        {
+            name: 'Braindead',
+            id: 2
+        }
     ];
   });
