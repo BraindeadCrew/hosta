810529d9dfda4e079988d44561173fc0142d7596
Maxime Tricoire <max.tricoire@gmail.com>
2013-11-30 15:47:05 +0100

    default unknown page handling

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,18 @@
 var main    = require('./main'),
     file    = require('./file'),
     recents = require('./recents'),
-    files   = require('./files');
+    files   = require('./files'),
+    thisisnotthepageyouarelookingfor = require('./thisisnotthepageyouarelookingfor');
 
 module.exports = function (app) {
-// GET routes
+  // GET routes
   app.get('/', main);
   app.get('/recents', recents);
   app.get('/:type/:id', files);
 
-// POST routes
+  // POST routes
   app.post('/file', file);
+
+  // Default route handling
+  app.get('/*', thisisnotthepageyouarelookingfor);
 }
