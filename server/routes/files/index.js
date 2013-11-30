192d4229bf2d348064a1b4c43e3ef146ef231adc
Maxime Tricoire <max.tricoire@gmail.com>
2013-11-30 16:07:36 +0100

    kick switch in files controller

--- server/routes/files/index.js
+++ server/routes/files/index.js
@@ -1,16 +1,12 @@
-var pic = require('./picture');
+var pic = require('./pic');
 
 module.exports = function (req, res) {
   var type = req.params.type;
 
-  switch (type) {
-    case 'pic':
-      return pic(req, res);
-
-    case 'vid':
-      return res.send(404, 'vid type not handled yet');
-
-    default:
-      return res.send(404, 'Unable to find requested route. Only "pic" is handled for now');
+  try {
+    require('./'+type)(req, res);
+    
+  } catch (err) {
+    return res.send(404, 'Unable to find requested route. Only "pic" is handled for now');
   }
 }
